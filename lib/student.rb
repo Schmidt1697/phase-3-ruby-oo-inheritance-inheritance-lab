a2e2689de458b4568d3f12ac81f239606822873d
Anna Schmidt <marshall1697@gmail.com>
2022-10-13 18:32:12 -0600

    commit tree 07b32358d8c459afff3d1362afb4ab136463d292

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+require 'pry'
+require_relative './user.rb'
 
+class Student < User
+
+    attr_accessor :knowledge
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(info)
+        self.knowledge << info
+    end
 end
+# student = Student.new('Mike', 'Jackson')
+binding.pry
